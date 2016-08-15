8c9ebf206522e2cefeecfe1f9b04a9899ba0bc35
Szemann Peter <szemannp@gmail.com>
2016-08-15 14:30:27 +0200

    sample data added

---
+++ husbands.js
@@ -0,0 +1,17 @@
+var participants = [
+    {
+      "id": 1,
+      "name": "Svejk",
+      "priority_list": [1, 3, 2]
+    },
+    {
+      "id": 2,
+      "name": "Zotya",
+      "priority_list": [2, 1, 3]
+    },
+    {
+      "id": 3,
+      "name": "Bastya elvtars",
+      "priority_list": [3, 2, 1]
+    }
+  ]
---
+++ wives.js
@@ -0,0 +1,20 @@
+var companies = [
+    {
+      "id": 1,
+      "name": "vidampark",
+      "priority_list": [1, 3, 2],
+      "participants_number": 1
+    },
+    {
+      "id": 2,
+      "name": "allatkert",
+      "priority_list": [2, 3, 1],
+      "participants_number": 1
+    },
+    {
+      "id": 3,
+      "name": "cirkusz",
+      "priority_list": [1, 2, 3],
+      "participants_number": 1
+    }
+]
